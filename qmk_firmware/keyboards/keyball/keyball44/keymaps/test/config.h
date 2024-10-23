a526a5acc3dedf2d109768e11b2e8a1778c7b9be
onoj9 <ngmt@yahoo.ne.jp>
2024-10-24 02:01:37 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/test/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/test/config.h
@@ -21,5 +21,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_RGB_TEST  // required for LED test
+//#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_SNAKE
+//#    define RGBLIGHT_EFFECT_KNIGHT
+//#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#    define RGBLIGHT_EFFECT_RGB_TEST
+//#    define RGBLIGHT_EFFECT_ALTERNATING
+//#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
+
+#define TAP_CODE_DELAY 5
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_LAYER_KEEP_TIME 30000
+
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 0
